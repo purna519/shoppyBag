8da4849edaf4aca9800baae0278564de9c0a3c21
Purna Chandra Rao <dhanekula591@gmail.com>
2025-10-07 16:46:34 +0530

    Initial commit

---
+++ src/main/java/com/shoppyBag/ShoppyBagApplication.java
@@ -0,0 +1,13 @@
+package com.shoppyBag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppyBagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppyBagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppyBag/ShoppyBagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppyBag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppyBagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
